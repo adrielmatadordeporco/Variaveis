56f50ba28ca2a5cc4722b7a725217f323de7a835
adriel <127214323+adrielmatadordeporco@users.noreply.github.com>
2024-08-29 01:29:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let anoAtual = 2024;
+const nomeCompleto = "Adriel Schwartz Ranucci";
+let idade = 17;
+const cursoDeFaculdade = "Psicologia";
+let cidadeAtual = "São José dos Pinhais";
+
+console.log (`olá, eu sou o ${nomeCompleto}, eu tenho ${idade} anos morando em ${cidadeAtual}, querendo cursar ${cursoDeFaculdade}`);
+
+anoAtual = 2025;
+
+console.log (anoAtual);
+
+idade = 18;
+cidadeAtual = "Fazenda";
+
+console.log (`olá novamente, como disse me chamo ${nomeCompleto}, 
+agora tenho ${idade} anos de idade, passando a morar em ${cidadeAtual}, e ainda querendo cursar ${cursoDeFaculdade}`);
