af491f0a8072ade58c2edaaa64d51e64f3085889
adriel <127214323+adrielmatadordeporco@users.noreply.github.com>
2024-09-19 01:35:13 +0000

    18/09/24

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ cidadeAtual = "Fazenda";
 
 console.log (`olá novamente, como disse me chamo ${nomeCompleto}, 
 agora tenho ${idade} anos de idade, passando a morar em ${cidadeAtual}, e ainda querendo cursar ${cursoDeFaculdade}`);
+
+//agência de viagens
+
+const nomeComprador = "Adalto";
+const CPFComprador = 10120341123;
+const idadeComprador = 23;
+const listaDestinos = new Array(
+  "Singapura",
+  " Tokio",
+  " Nova York",
+  " Paris",
+  " Toronto"
+);
+
+console.log(`olá sr. ${nomeComprador}, portador do CPF: ${CPFComprador} e idade de: ${idadeComprador} anos, possui a lista : ${listaDestinos}.`);
+
+console.log(listaDestinos[2]);
+
+console.log("");
+console.log(`olá sr. ${nomeComprador}, você terá um novo destino que será incluido na lista a seguir: `);
+
+listaDestinos.push("Londres");
+console.log(listaDestinos);
